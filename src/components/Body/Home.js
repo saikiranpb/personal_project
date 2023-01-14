ab498bcaf9bcc126ce5c42de785fa09ad7bd1966
Sai Kiran P B <saikiran07144@gmail.com>
2023-01-14 11:05:32 +0530

    Update Home.js

--- src/components/Body/Home.js
+++ src/components/Body/Home.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
 export default function Home() {
+
   return (
     <div style={{display:'flex',height:'50px',fontWeight:'bold',padding:'10px 30px'}}>
-        Home
+      <div>
+        <div>
+          <h2>Hats</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
     </div>
   )
 }
